2b2aaec59baf18776c7ea4ec6ef0dd358264d1ed
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2017-01-30 20:20:50 +0000

    Adde merge sort

---
+++ MergeSort.Tests/MergeSortTests/Sort.cs
@@ -0,0 +1,24 @@
+﻿namespace MergeSort.Tests.MergeSortTests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Sort
+    {
+        [TestCase(new[] { 2, 1, 5 })]
+        [TestCase(new[] { 2, 1, 5, 9 })]
+        [TestCase(new[] { 2, 1, 5, 9, 2, 5, 14 })]
+        [TestCase(new[] { 77, -11, 0, 44, 99, 22, 88, 66, 55, 11, 33 })]
+        public void Successfully(int[] array)
+        {
+            // Arrange
+            var mergeSort = new MergeSort(array);
+
+            // Act
+            mergeSort.Sort();
+
+            // Assert
+            CollectionAssert.IsOrdered(array);
+        }
+    }
+}
---
+++ MergeSort/MergeSort.cs
@@ -0,0 +1,74 @@
+﻿namespace MergeSort
+{
+    using System.Collections.Generic;
+
+    public class MergeSort
+    {
+        private readonly int[] _array;
+
+        public MergeSort(int[] array)
+        {
+            _array = array;
+        }
+
+        public void Sort()
+        {
+            var workspace = new int[_array.Length];
+            RecursiveMergeSort(workspace, 0, _array.Length - 1);
+        }
+
+        private void RecursiveMergeSort(IList<int> workspace, int lowerBound, int upperBound)
+        {
+            // Pivot point
+            if (lowerBound == upperBound)
+                return;
+
+            // Get mid
+            var mid = (lowerBound + upperBound) / 2;
+
+            // Sort recursively the lower part of the array
+            RecursiveMergeSort(workspace, 0, mid);
+
+            // Sort recursively the upper part of the array
+            RecursiveMergeSort(workspace, mid + 1, upperBound);
+
+            // Merge them
+            Merge(workspace, lowerBound, mid + 1, upperBound);
+        }
+
+        private void Merge(IList<int> workspace, int lowPtr, int highPtr, int upperBound)
+        {
+            int j = 0,
+                lowerBound = lowPtr,
+                mid = highPtr - 1,
+                length = upperBound - lowerBound + 1;
+
+            // Sort upper and lower part
+            while (lowPtr <= mid && highPtr <= upperBound)
+            {
+                if (_array[lowPtr] < _array[highPtr])
+                    workspace[j++] = _array[lowPtr++];
+                else
+                    workspace[j++] = _array[highPtr++];
+            }
+
+            // If still haven't finished with lower part, do your job
+            while (lowPtr <= mid)
+            {
+                workspace[j++] = _array[lowPtr++];
+            }
+
+            // If still haven't finished with upper part, do your job
+            while (highPtr <= upperBound)
+            {
+                workspace[j++] = _array[highPtr++];
+            }
+
+            // Put workspace, which is sorted into the actual array
+            for (j = 0; j < length; j++)
+            {
+                _array[lowerBound + j] = workspace[j];
+            }
+        }
+    }
+}
