af66f9b1600d2c930d5955cfed8940ed2cd8463b
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2017-05-20 00:02:11 +0100

    Updated folders

---
+++ Patterns.Observer.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Observer;
+
+namespace Patterns.Observer.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var subscriber1 = new Subscriber();
+            var subscriber2 = new Subscriber();
+
+            var publisher = new Publisher(new List<ISubscriber>
+            {
+                subscriber1, subscriber2
+            });
+
+            var message = "Hello world";
+            publisher.Message = message;
+
+            publisher.Message = "Hello universe!";
+
+            Console.ReadLine();
+        }
+    }
+}
