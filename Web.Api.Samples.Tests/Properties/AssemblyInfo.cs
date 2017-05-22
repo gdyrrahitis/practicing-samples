b854069c3f8a58c87468d61b5d24037db3ec8467
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2017-05-22 22:17:25 +0100

    Updated web api project

--- Web.Api.Samples.Tests/Properties/AssemblyInfo.cs
+++ Web.Api.Samples.Tests/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApiToTestsOn.Tests")]
+[assembly: AssemblyTitle("Web.Api.Samples")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
--- Web.Api.Samples/Properties/AssemblyInfo.cs
+++ Web.Api.Samples/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApiToTestsOn")]
+[assembly: AssemblyTitle("Web.Api.Samples")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApiToTestsOn")]
+[assembly: AssemblyProduct("Web.Api.Samples")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
